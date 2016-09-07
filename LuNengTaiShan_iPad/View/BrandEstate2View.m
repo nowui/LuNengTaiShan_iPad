5b77e2f8a1144707ad376c0f10e21ae42f19e39a
NowUI <nowui@163.com>
2016-09-08 03:13:01 +0800

    2016-09-08 03:12

--- LuNengTaiShan_iPad/View/BrandEstate2View.m
+++ LuNengTaiShan_iPad/View/BrandEstate2View.m
@@ -8,16 +8,42 @@
 
 #import "BrandEstate2View.h"
 
+@interface BrandEstate2View () {
+    UIImageView *backgroundimageView;
+    UIButton *leftButton;
+    UIButton *rightButton;
+}
+
+@end
+
 @implementation BrandEstate2View
 
 - (id)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 1024, 768)];
-        [imageView setImage:[UIImage imageNamed:[NSString stringWithFormat:@"brand_estate_%d.png", 2]]];
-        [self addSubview:imageView];
+        backgroundimageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 1024, 768)];
+        [backgroundimageView setImage:[UIImage imageNamed:[NSString stringWithFormat:@"brand_estate_%d.png", 2]]];
+        [self addSubview:backgroundimageView];
+        
+        leftButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [leftButton setFrame:CGRectMake(512, 689, 27, 24)];
+        [leftButton addTarget:self action:@selector(clickLeftButton:) forControlEvents:UIControlEventTouchUpInside];
+        [self addSubview:leftButton];
+        
+        rightButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [rightButton setFrame:CGRectMake(485, 77, 27, 24)];
+        [rightButton addTarget:self action:@selector(clickRightButton:) forControlEvents:UIControlEventTouchUpInside];
+        [self addSubview:rightButton];
     }
     return self;
+}
+
+- (void)clickLeftButton:(id)sender {
+    
+}
+
+- (void)clickRightButton:(id)sender {
+    
 }
 
 @end
